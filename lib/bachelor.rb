eb391a1167d9641a8231398a4c5b21213fe1c496
Tara M. Harwood <tara.m.harwood@gmail.com>
2021-02-02 17:30:10 -0800

    refactored name of season winner to use .filter method

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,16 +5,23 @@ file = File.read('./lib/contestants.json')
 contestants = JSON.parse(file)
 # p contestants
 
-def get_first_name_of_season_winner(data, season)
+def OLD_get_first_name_of_season_winner(data, season)
   winner = ''
-  season_data = data.find { | key, index | key == season }  #returns the correct season
-  season_data[1].each do | person | #loops over the people - I'm not thrilled with that [1] ... is there a better enumeratior?
+  season_data = data.find { | key, value | key == season }  #returns the correct season
+  season_data[1].each do | person | #loops over the people - I'm not thrilled with that [1] ... is there a better way?
     if person["status"] == "Winner"
       winner = person["name"].split.first
     end  
   end
   return winner    
-end  
+end 
+
+def get_first_name_of_season_winner(data, season)
+  winner = data[season].filter { | person | person["status"] == "Winner" }
+  winner[0]["name"].split.first
+end
+
+get_first_name_of_season_winner(contestants, "season 10")
 
 
 ## better figure out how to use .map!
@@ -66,16 +73,17 @@ end
 def get_average_age_for_season(data, season)
   combined_age = 0
   average_age = 0
-  season_data = data.find { | key, index | key == season }  #returns the correct season
+  season_data = data.find { | key, value | key == season }  #returns the correct season
   season_data[1].each do | person |
     combined_age += person["age"].to_i
   end
   # p combined_age
   # p season_data[1].length
-  # p (combined_age.to_f / season_data[1].length.to_f).round()
+  p (combined_age.to_f / season_data[1].length.to_f).round()
   return (combined_age.to_f / season_data[1].length.to_f).round()
 end
 
-get_average_age_for_season(contestants, "season 10")
 
-#this is a test of the emergency broadcast system.  This is only a test.
+
+
+
